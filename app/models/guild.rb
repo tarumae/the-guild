a0c516b7c9018cc93ca99c42cdba9d9fbb785393
Toomas Arumae <tom.arumae@gmail.com>
2021-03-02 11:40:42 +0000

    Added presence validation for category (unsure if necessary)

--- app/models/guild.rb
+++ app/models/guild.rb
@@ -5,7 +5,7 @@ class Guild < ApplicationRecord
   has_many :milestones
 
   CATEGORIES = %w[Family Work Gaming Sports Education General]
-  validates :category, inclusion: { in: CATEGORIES }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :name, presence: true, length: { minimum: 3, maximum: 60 }
   validates :user, presence: true
 end
