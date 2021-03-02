4608d865622a5ca86414d59360c3d75399a5b348
Daniele Argenti <daniele.argenti@gmail.com>
2021-03-02 18:14:16 +0000

    updated new.html.erb, routes and posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,31 +1,51 @@
 class PostsController < ApplicationController
-  before_action :skip_authorization, only: [:new, :create]
+  before_action :skip_authorization, only: [:index, :new, :create]
 
+  def index
+    @guild = Guild.find(params[:guild_id])
+    @posts = @guild.posts
+  end
+
+  def new
+    @post = Post.new
+    @guild = Guild.find(params[:guild_id])
+    # authorize @post
+  end
+  
+  def create
+    @guild = Guild.find(params[:guild_id])
+    @post = Post.new(post_params)
+    @post.user = current_user
+    # authorize @post
+    if @post.save
+      # flash[:notice] = "Post successfully created!"
+      redirect_to guild_posts_path(@guild) #ask Lenny guild_posts_path(@post)
+    else
+      flash[:alert] = "Something went wrong, please try again. If this issue persists, please contact us for help."
+      render :new
+    end
+  end
+  
   def edit
     @post = Post.find(params[:id])
     authorize @post
   end
 
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.new(post_params)
-    @post.save
-  end
-  
   def update
     
   end
-
-  def destroy
   
+  def destroy
+    @post = Post.find(params[:id]) # do we need this?
+    authorize @post
+    @post.destroy
+    flash[:notice] = "Post successfully destroyed!"
+    redirect_to guild_path(@guild)
   end
 
   private
 
   def post_params
-    params.require(:guild).permit(:desription)
+    params.require(:post).permit(:description)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-<<<<<<< HEAD
-<<<<<<< HEAD
-  resources :guilds, only: :show do
-    resources :posts, only: [:new, :create, :edit, :update, :destroy]
-  end
-=======
-  resources :guilds
->>>>>>> master
-=======
-
   resources :guilds do
-    resources :posts, only: [:new, :create, :edit, :update, :destroy]
+    resources :posts 
   end
->>>>>>> master
+
 end
