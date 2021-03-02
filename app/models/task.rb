b8bcc115acca897abfe25fd0947a861a4d5f07f9
jihye kang <jihye59kang@gmail.com>
2021-03-02 12:10:23 +0000

    task validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :user
   belongs_to :milestone
+
+  validates :title, :description, presence: true
+  validates :title, length: { minimum: 3,
+    too_short: "minimum is %{count} characters" }
 end
