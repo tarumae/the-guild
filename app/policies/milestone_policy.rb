d1783cf15a792b6e756ad5062f1b48c6dd804c55
jihye kang <jihye59kang@gmail.com>
2021-03-02 17:27:11 +0000

    milestone create page

--- app/policies/milestone_policy.rb
+++ app/policies/milestone_policy.rb
@@ -3,9 +3,18 @@ class MilestonePolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+  end
+  def show?
+    record.user == user || record.guild_users.where(user: user).present?
+  end
 
-    def show?
-      record.user == user
-    end
+  def create?
+    true
+  end
+
+  def new?
+    # checking if user is admin
+    # user.guild_user.admin
+    true
   end
 end
