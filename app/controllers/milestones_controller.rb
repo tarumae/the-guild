d02a2948099f27d4a4f5dcc9849acbf56f2d8755
jihye kang <jihye59kang@gmail.com>
2021-03-02 14:38:44 +0000

    milestone show page

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -1,2 +1,13 @@
 class MilestonesController < ApplicationController
+before_action :set_allotment, only: :show
+  def show
+    @tasks = current_user.guilds.milestones.tasks
+  end
+
+  private
+
+  def set_milestone
+    @milestone = Milestone.find(params[:id])
+    authorize @milestone
+  end
 end
--- app/policies/milestone_policy.rb
+++ app/policies/milestone_policy.rb
@@ -3,5 +3,9 @@ class MilestonePolicy < ApplicationPolicy
     def resolve
       scope.all
     end
+
+    def show?
+      true
+    end
   end
 end
