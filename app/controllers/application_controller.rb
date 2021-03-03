bfdb324579f65bf3f5431dffe0ca2710350a9973
Toomas Arumae <tom.arumae@gmail.com>
2021-03-03 11:33:57 +0000

    Add index method for guilds, only returns the guild the user owns or is a part of

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  def after_sign_in_path_for(*)
+    guilds_path
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name nickname])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name nickname about])
