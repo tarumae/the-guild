3d96d90d5732a6ebe108bfbacb56c959cd10b6fb
Daniele Argenti <daniele.argenti@gmail.com>
2021-03-03 11:41:04 +0000

    sorted post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,25 +1,28 @@
 class PostsController < ApplicationController
-  before_action :skip_authorization, only: [:index, :new, :create]
+  before_action :skip_authorization, only: [:index]
+  before_action :set_post, only: [:edit, :update, :destroy]
 
   def index
     @guild = Guild.find(params[:guild_id])
-    @posts = @guild.posts
+    @posts = @guild.posts.order(created_at: :desc)
   end
 
   def new
     @post = Post.new
     @guild = Guild.find(params[:guild_id])
-    # authorize @post
+    @post.guild = @guild
+    authorize @post
   end
   
   def create
     @guild = Guild.find(params[:guild_id])
     @post = Post.new(post_params)
+    @post.guild = @guild
+    authorize @post
     @post.user = current_user
-    # authorize @post
     if @post.save
-      # flash[:notice] = "Post successfully created!"
-      redirect_to guild_posts_path(@guild) #ask Lenny guild_posts_path(@post)
+      flash[:notice] = "Post successfully created!"
+      redirect_to guild_posts_path(@guild) 
     else
       flash[:alert] = "Something went wrong, please try again. If this issue persists, please contact us for help."
       render :new
@@ -27,23 +30,30 @@ class PostsController < ApplicationController
   end
   
   def edit
-    @post = Post.find(params[:id])
-    authorize @post
   end
 
   def update
-    
+    if @post.update(post_params)
+      redirect_to guild_posts_path(@post.guild)
+    else
+      render :edit
+    end
   end
   
   def destroy
-    @post = Post.find(params[:id]) # do we need this?
-    authorize @post
+    @guild = @post.guild
     @post.destroy
     flash[:notice] = "Post successfully destroyed!"
-    redirect_to guild_path(@guild)
+    redirect_to guild_posts_path(@post.guild)
   end
 
   private
+
+  def set_post
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+  
 
   def post_params
     params.require(:post).permit(:description)
