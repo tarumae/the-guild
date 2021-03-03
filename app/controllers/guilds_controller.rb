8d165e72c5ce1044965fa586b46045bae72073d4
Toomas Arumae <tom.arumae@gmail.com>
2021-03-03 12:18:16 +0000

    Add edit/update action for guilds controller

--- app/controllers/guilds_controller.rb
+++ app/controllers/guilds_controller.rb
@@ -1,12 +1,10 @@
 class GuildsController < ApplicationController
+  before_action :set_guild, only: %i[show edit update destroy]
   def index
     @guilds = policy_scope(Guild)
   end
 
-  def show
-    @guild = Guild.find(params[:id])
-    authorize @guild
-  end
+  def show; end
 
   def new
     @guild = Guild.new
@@ -26,7 +24,24 @@ class GuildsController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @guild.update(guild_params)
+      flash[:notice] = "Guild successfully updated!"
+      redirect_to guild_path(@guild)
+    else
+      flash[:alert] = "Something went wrong, please try again. If this issue persists, please contact us for help."
+      render :edit
+    end
+  end
+
   private
+
+  def set_guild
+    @guild = Guild.find(params[:id])
+    authorize @guild
+  end
 
   def guild_params
     params.require(:guild).permit(:name, :description, :category)
