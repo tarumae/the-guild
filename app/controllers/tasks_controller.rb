abf35a97cd5362c8193cf6709379afbee57e15e7
jihye kang <jihye59kang@gmail.com>
2021-03-04 14:37:56 +0000

    update task/ add modal form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+
   def index
     @milestone = Milestone.find(params[:milestone_id])
     @tasks = @milestone.tasks
@@ -17,14 +18,34 @@ class TasksController < ApplicationController
       end
   end
 
+  def update
+    #As a user I can tick a task as done
+    @milestone = Milestone.find(params[:milestone_id])
+    @task = Task.find(params[:id])
+    authorize @task
+    @task.completed = params[:completed]
+      if @task.completed
+        if @task.save
+          flash[:notice] = "#{@task.title} is now completed!"
+          redirect_to guild_milestone_path(@milestone.guild, @milestone)
+        else
+          flash[:alert] =
+            "Something went wrong. Please try again later. If this issue persists, please contact the support team."
+        end
+      elsif @task.completed == false
+        if @task.save
+          flash[:notice] = "#{@task.title} is still in progress!"
+          redirect_to guild_milestone_path(@milestone.guild, @milestone)
+        else
+          flash[:alert] =
+            "Something went wrong. Please try again later. If this issue persists, please contact the support team."
+        end
+    end
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :description, :user_id)
-  end
-
-  def set_task
-    @task = Task.find(params[:id])
-    authorize @task
   end
 end
