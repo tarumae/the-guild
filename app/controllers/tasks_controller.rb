5af330106b95d4ccbb54dfe9617360d9332196a0
Toomas Arumae <tom.arumae@gmail.com>
2021-03-04 17:39:04 +0000

    Minor bugfixes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,4 @@
 class TasksController < ApplicationController
-
   def index
     @milestone = Milestone.find(params[:milestone_id])
     @tasks = @milestone.tasks
@@ -10,21 +9,19 @@ class TasksController < ApplicationController
     @task = Task.new(task_params)
     @task.milestone = @milestone
     authorize @task
-      if @task.save
-        flash[:notice] = "Task successfully created!"
-        redirect_to guild_milestone_path(@milestone.guild, @milestone)
-      else
-        flash[:alert] = "Something went wrong, please try again. If this issue persists, please contact us for help."
-      end
+    if @task.save
+      flash[:notice] = "Task successfully created!"
+      redirect_to guild_milestone_path(@milestone.guild, @milestone)
+    else
+      flash[:alert] = "Something went wrong, please try again. If this issue persists, please contact us for help."
+    end
   end
 
   def update
-    #As a user I can tick a task as done
+    # As a user I can tick a task as done
     @milestone = Milestone.find(params[:milestone_id])
     @task = Task.find(params[:id])
     authorize @task
-    puts task_params
-    puts "XXXXXXXXXXXXXZXZX"
     if @task.update(task_params)
       flash[:notice] = "#{@task.title} is now #{@task.completed ? 'completed' : 'in progress'}!"
       redirect_to guild_milestone_path(@milestone.guild, @milestone)
