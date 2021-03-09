192df2e7bce044d10f4d4b451acb6e8985bb9190
Toomas Arumae <tom.arumae@gmail.com>
2021-03-09 12:40:16 +0000

    Only the assigned person can mark the task as complete now.

--- app/controllers/milestones_controller.rb
+++ app/controllers/milestones_controller.rb
@@ -19,7 +19,7 @@ class MilestonesController < ApplicationController
     authorize @milestone
     if @milestone.save
       flash[:notice] = "Milestone successfully created!"
-      redirect_to guild_milestones_path
+      redirect_to guild_milestone_path(@milestone.guild, @milestone)
     else
       flash[:alert] = "Something went wrong, please try again. If this issue persists, please contact us for help."
       render :new
