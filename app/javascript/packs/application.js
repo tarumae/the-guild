2fc73742f1ebc52373773c1426cb9c8b1d6fe30c
Toomas Arumae <tom.arumae@gmail.com>
2021-03-09 13:22:23 +0000

    Fixed post not working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("trix")
+require("@rails/actiontext")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -33,5 +35,3 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
 });
 
-require("trix")
-require("@rails/actiontext")
